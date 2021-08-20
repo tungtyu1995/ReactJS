236fd84ad4ab0d1c080fdc19614b7263ca71db4a
tungtyu1995 <tusytung@gmail.com>
2021-08-20 17:34:33 +0700

    tung commit

---
+++ src/features/Todu/components/TodoList/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+TodoList.propTypes = {
+    todoList: PropTypes.array,
+
+};
+TodoList.defaultProp = {
+    todoList: [],
+}
+
+function TodoList({ todoList }) {
+
+    return (
+        <ul>
+            {todoList.map(todo => (
+                <li key={todo.id}>{todo.title}</li>
+            ))}
+        </ul>
+    );
+}
+
+export default TodoList;
